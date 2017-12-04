4167099e3b0b3512ba85b771cedbcae9fd5ff234
OHTA Shogo <athos0220@gmail.com>
2017-12-05 00:55:08 +0900

    Add solution for Day 3 Part 2

--- src/advent_of_code_2017/day_3.clj
+++ src/advent_of_code_2017/day_3.clj
@@ -17,3 +17,40 @@
                   (map #(Math/abs (long (- % n))))
                   (apply min))]
     (+ i diff)))
+
+(defn- next-cell [cells [y x]]
+  (case [(boolean (get cells [y (+ x 1)]))
+         (boolean (get cells [(+ y 1) x]))
+         (boolean (get cells [y (- x 1)]))
+         (boolean (get cells [(- y 1) x]))]
+    ([true  false false false]
+     [true  true  false false])
+    [(- y 1) x]
+
+    ([false true  false false]
+     [false true  true  false])
+    [y (+ x 1)]
+
+    ([false false true  false]
+     [false false true  true ])
+    [(+ y 1) x]
+
+    ([false false false true ]
+     [true  false false true ])
+    [y (- x 1)]))
+
+(defn- fill-cell [cells [y x :as pos]]
+  (->> (for [dy [-1 0 1]
+             dx [-1 0 1]
+             :when (and (not (and (= dy 0) (= dx 0))))]
+         (get cells [(+ y dy) (+ x dx)] 0))
+       (apply +)
+       (assoc cells pos)))
+
+(defn solve2 [n]
+  (loop [cells {[0 0] 1}, pos [0 1]]
+    (let [cells (fill-cell cells pos)
+          cell (get cells pos)]
+      (if (> cell n)
+        cell
+        (recur cells (next-cell cells pos))))))
--- test/advent_of_code_2017/day_3_test.clj
+++ test/advent_of_code_2017/day_3_test.clj
@@ -7,3 +7,8 @@
   (is (= 3 (solve1 12)))
   (is (= 2 (solve1 23)))
   (is (= 31 (solve1 1024))))
+
+(deftest solve2-test
+  (is (= 122 (solve2 100)))
+  (is (= 304 (solve2 300)))
+  (is (= 806 (solve2 800))))
