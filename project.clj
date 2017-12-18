8302389ef0c5a0fc71a6c299fb95de87769abca1
OHTA Shogo <athos0220@gmail.com>
2017-12-18 22:44:17 +0900

    Add some other deps for Spec

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies
+                   [[pinpointer "0.1.0"]
+                    [org.clojure/test.check "0.10.0-alpha2"]]}})
